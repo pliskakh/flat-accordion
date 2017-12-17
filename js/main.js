832979391aca90a91aed92861afa06f6d7a50de5
Mykhailo Pliskunov <pliskunov.mykhailo@gmail.com>
2017-12-17 23:02:00 +0200

    added logic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
 ;(function(){
 	"use strict";
-	
+
+	$(document).ready(function(){
+		$('.ba-item__description').not(':first').hide();
+		$('.ba-item__title').click(function(){
+
+			var description = $(this).next('.ba-item__description');
+			var accordion = $(this).closest('.ba-accordion');
+
+			if(description.is(':visible')){
+				description.slideUp(500);
+			} else {
+				accordion.find(' .ba-item__description').slideUp(500);
+				description.slideDown(400);
+			}
+
+		});
+	});
+
+
 })();
